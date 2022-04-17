46d734824ddbd5df273756708558cb96967a3554
HedgehogKU <ccc908925@gmail.com>
2022-04-17 20:11:47 +0800

    測試啟動 node server

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log('hello');
+const http = require('http');
+
+const PORT = 3005;
+
+const requestListener = (req, res) => {
+    console.log(req.url, req.method);
+    res.end();
+}
+
+const server = http.createServer(requestListener);
+
+server.listen(PORT, () => {
+    if ( process.env.PORT || PORT ) {
+        console.log('Server is running...');
+    }
+});
