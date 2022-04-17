9ec1c0cbd02a9ba32d73ffcf5f22194f7f16dfe4
HedgehogKU <ccc908925@gmail.com>
2022-04-17 20:44:49 +0800

    新增 router: POST /posts

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ mongoose
     .catch((err) => console.log(err.message));
 
 const requestListener = async (req, res) => {
+
+    let body = '';
+    req.on('data', (chunk) => {
+        body += chunk;
+    });
+
     if ( req.url === '/' && req.method === 'GET' ) {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write('<h1>Home Page</h1>');
@@ -23,6 +29,40 @@ const requestListener = async (req, res) => {
             data,
         }));
         res.end();
+    } else if ( req.url === '/posts' && req.method === 'POST' ) {
+        req.on('end', async () => {
+            try {
+                const { content } = JSON.parse(body);
+                if ( !content ) {
+                    res.writeHead(400, {'Content-Type': 'application/json'});
+                    res.write(JSON.stringify({
+                        result: false,
+                        msg: '【內容】必填',
+                    }));
+                    res.end();
+                    return;
+                }
+
+                const data = await PostModel.create(
+                    {
+                        content,
+                    }
+                );
+                res.writeHead(200, {'Content-Type': 'application/json'});
+                res.write(JSON.stringify({
+                    result: true,
+                    data,
+                }));
+                res.end();
+            } catch(err) {
+                res.writeHead(400, {'Content-Type': 'application/json'});
+                res.write(JSON.stringify({
+                    result: false,
+                    msg: err.message,
+                }));
+                res.end();
+            }
+        });
     } else if ( req.method === 'OPTIONS' ) {
         res.writeHead(200);
         res.end();
